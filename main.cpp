9b4d9de64724b9cd5405e2e6a49e43c71c7788dc
sbc <sbc@skynet.lan>
2025-04-22 01:10:21 -0700

    hang person game linux - ncurses

---
+++ main.cpp
@@ -0,0 +1,84 @@
+/* pager functionality by Joseph Spainhour" <spainhou@bellsouth.net> */
+#include <ncurses.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{ 
+  initscr();	
+  noecho();
+  //cbreak();
+
+  const char* wordlist[3];
+  wordlist[0] = "string 1";
+  wordlist[1] = "string 2";
+  wordlist[2] = "string 3";
+  int choice = 0;
+  int highlight = 0;
+  int wordlistsize = sizeof(wordlist);
+  int elementsize = sizeof(wordlist[0]);
+  int listsizemax = wordlistsize / elementsize;
+  
+  //scren size
+  int y, x = 0;
+  getmaxyx(stdscr, y, x);
+
+  //window test
+  WINDOW* renderwindow = newwin(20, 50, 3, 3);
+  box(renderwindow, 0, 0);
+  refresh();
+  wrefresh(renderwindow);
+
+  //create menu window
+  WINDOW* inputwin = newwin(6, x-12, y-8, 5);
+  box(inputwin, 0, 0);
+  refresh();
+  wrefresh(inputwin);
+  keypad(inputwin, true);
+  curs_set(0);
+
+  while(1)
+  {
+    for(int i = 0; i < 3; i++)
+    {
+      if(i == highlight)
+      {
+        wattron(inputwin, A_REVERSE);
+      }
+      mvwprintw(inputwin, i+1, 1, wordlist[i]);
+      wattroff(inputwin, A_REVERSE);
+    }
+
+    choice = wgetch(inputwin);
+
+    switch(choice)
+    {
+      case KEY_UP:
+        if(highlight - 1 < 0)
+        {
+          break;
+        }
+        highlight--;
+        break;
+      case KEY_DOWN:
+        if(highlight + 1 > (listsizemax - 1))
+        {
+          break;
+        }
+        highlight++;
+        break;
+      defeault:
+        break;
+    }
+
+    if(choice == 10)
+    {
+      break;
+    }
+  }
+
+
+  getch();
+  endwin();
+
+  return 0;
+}                                         
